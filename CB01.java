91b29078b7cb06261628adcea6ee716569c4d7bf
TienDuong <ltd08102002@gmail.com>
2022-12-30 22:33:34 +0700

    them

---
+++ CB01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author Laptop
+ */
+public class CB01  {
+    public static void main(String[] args) {
+        System.out.println("Lap Trinh Khong Kho!");
+    }
+    
+}
---
+++ CB02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Laptop
+ */
+public class CB02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println(a+b);
+    }
+}
